f6ebc734e8db5751845fc55580388b98260b1597
Pooya <py.assadi@gmail.com>
2025-03-13 02:45:23 +0330

    React example2b finished

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="quizContainer">
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
